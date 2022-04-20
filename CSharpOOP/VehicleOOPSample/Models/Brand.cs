52f3bbd7662dc93a467978fe6781dc4fab0e7b65
cagatayyildiz <yildiz.cagatay@hotmail.com>
2022-04-20 11:15:57 +0300

    vehicle sample

---
+++ CSharpOOP/VehicleOOPSample/Models/Brand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace VehicleOOPSample.Models
+{
+    public class Brand
+    {
+        public Brand(string name)
+        {
+            this.Name = name;
+        }
+        
+
+        public string Name { get; set; }
+        public string Logo { get; set; }
+    }
+}
---
+++ CSharpOOP/VehicleOOPSample/Models/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace VehicleOOPSample.Models
+{
+
+//Id -> int
+//Brand -> Class
+//Year -> Int
+
+//CalcVisaDate protected virtual metot(int alır int döner. Dışarıdan aldığı yılın 10 sene sonrasını döner)
+
+
+
+    public class Vehicle
+    {
+        public int Id { get; set; }
+
+        public Brand Brand { get; set; }
+
+        public int Year { get; set; }
+
+        protected virtual int CalcVisaDate(int year)
+        {
+            return year + 10;
+        }
+    }
+}
