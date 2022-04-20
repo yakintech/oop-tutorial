550827274a38f368fdc8e37ef1a76a59f3a1abe4
cagatayyildiz <yildiz.cagatay@hotmail.com>
2022-04-20 14:26:57 +0300

    bug fix

---
+++ VehicleOOPSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VehicleOOPSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
