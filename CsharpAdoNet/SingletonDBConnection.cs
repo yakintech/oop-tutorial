1e0413cc77cb7c4ddaee324947d99d92bf68b983
cagatayyildiz <yildiz.cagatay@hotmail.com>
2022-04-28 09:29:24 +0300

    singleton start

---
+++ CsharpAdoNet/SingletonDBConnection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SqlClient;
+using CsharpAdoNet.Env;
+
+namespace CsharpAdoNet
+{
+    public class SingletonDBConnection
+    {
+        private static SingletonDBConnection dbInstance;
+
+        private readonly SqlConnection sqlConnection = new SqlConnection(Connection.connectionString);
+
+
+        private SingletonDBConnection()
+        {
+
+        }
+
+        public static SingletonDBConnection getDbInstance()
+        {
+            if (dbInstance == null)
+            {
+                dbInstance = new SingletonDBConnection();
+            }
+
+            return dbInstance;
+        }
+        
+    }
+}
